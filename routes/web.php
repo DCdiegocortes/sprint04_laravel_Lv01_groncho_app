e43edffb35d84e6411449272b634d3093ab89700
Diego Cortes <dc.diegocortes@gmail.com>
2026-09-06 20:14:34 +0200

    feat: add universe store route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UniverseController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +16,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/universe/create', [UniverseController::class, 'create'])->name('universe.create');
+    Route::post('/universe', [UniverseController::class, 'store'])->name('universe.store');
 });
 
 require __DIR__.'/auth.php';
